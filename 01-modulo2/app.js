9544152521e75674fb3ebba84e6e243e68bc1281
carrizosan <santiicaarrizo@gmail.com>
2021-07-19 20:21:14 -0300

    tarea 5, npm

--- 01-modulo2/app.js
+++ 01-modulo2/app.js
@@ -1,12 +1,14 @@
 const fileSystem = require("./utilities/writeFile");
 const table = require("./utilities/table");
+var colors = require("colors");
 let result = "";
 
 const base = process.argv[2].split("--base=")[1];
-const limit = process.argv[3]?.split("--limit=")[1]; // Segundo parametro Limit opcional.
+const limit = process.argv[3]?.split("--limit=")[1] || 10; // Segundo parametro Limit opcional.
 
 result = table.createTableByBase(result, base, limit);
 
+console.log(`TABLA DEL ${base} HASTA EL NUMERO ${limit} !!`.rainbow);
 console.log(result);
 
 const fileName = `tabla${base}.txt`;
